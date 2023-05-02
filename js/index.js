99a120c5bdd7cde4e694f9b9d4269c889c6736fb
Alja Čekada <alja.cekada@gmail.com>
2023-05-02 20:10:23 +0200

    create navbar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
+const menuBtn = document.querySelector(".menu-btn")
+const navigation = document.querySelector(".navigation")
+const closeNavBtn = document.querySelector(".navigation-close-btn")
+const blurOverlay = document.querySelector(".blur-overlay")
+
+const IS_ACTIVE = "is--active"
+
+const toggleNavigation = () => {
+    navigation.classList.toggle(IS_ACTIVE)
+    blurOverlay.classList.toggle(IS_ACTIVE)
+}
+
+const CLICK = "click"
+
+menuBtn.addEventListener(CLICK, toggleNavigation)
+closeNavBtn.addEventListener(CLICK, toggleNavigation)
+
 new fullpage('#fullpage', {autoScrolling: true, scrollbar: true})
+
